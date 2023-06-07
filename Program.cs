735be70a81483792900b16510333d38e7ec6f05f
Emilytoledo <135863667+Emilytoledo@users.noreply.github.com>
2023-06-07 15:27:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int numero;
+
+            Console.WriteLine("Digite um número de 1 a 10");
+            numero = int.Parse(Console.ReadLine());
+            if (numero<= 10)
+            {
+
+                Console.WriteLine("Opa! Digite um número válido...");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Opa! Digite um número válido...");
+
+
+
+            Console.WriteLine("- FIM DO PROGRAMA");
+            
+
+
+            }
+
+
+
+
+           Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
